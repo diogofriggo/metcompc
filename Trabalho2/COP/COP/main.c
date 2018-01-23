8580f7450b99faa9ba18149bdc6433bdd0c2fb9f
diogofriggo <diogofriggobr@gmail.com>
2018-01-23 02:37:09 -0200

    Unable to properly implement suggested boundary conditions

--- Trabalho2/COP/COP/main.c
+++ Trabalho2/COP/COP/main.c
@@ -8,19 +8,34 @@
 void run();
 void setup();
 void iterate();
+void attemptFlip(int i, int j, int in, int jn);
+int computePairEnergy(int i, int j, int in, int jn);
+int computeEnergy(int i, int j, int in, int jn);
 void report(FILE *file);
 void setup();
 void timeit(void (*fun)(void));
-int randomIntegerInclusive(int lowerLimit, int upperLimit);
+double randomDoubleInclusive(double lowerLimit, double upperLimit);
 
+const double rho = 0.5;
 const int l = 50;
 const int l2 = l/2;
-const int iterations = 1000;
+const int iterations = 100;
+const int J = 1;
+const int kB = 1;
+const int T = 1;
+//beta >= 187 forbids any transition
+//changing boundary conditions beta >= 94 forbids any transition
+const int beta = 1000000000;//1/(kB*T);
+
 int lattice[l][l];
 
 int main() {
     srand((unsigned int)time(NULL));
     timeit(run);
+    int i;
+    for(i = 0; i < 100; i++){
+        printf("%.2f ", randomDoubleInclusive(0.,1.));
+    }
     return 0;
 }
 
@@ -32,7 +47,7 @@ void run(){
     FILE *file = fopen(path, "w");
     for(i = 0; i < iterations; i++){
         iterate();
-        if(i%10==0)
+//        if(i%10==0)
             report(file);
     }
     fclose(file);
@@ -44,21 +59,55 @@ void setup()
     
     for(i = 0; i < l; i++)
         for(j = 0; j < l; j++)
-            lattice[i][j] = 0;
+            lattice[i][j] = -1;
     
     for(i = 0; i < l; i++)
         for(j = 0; j < l2; j++)
             lattice[i][j] = 1;
 }
 
+//your neighbours are (top,right,bottom,left) = (i,j+1) (i+1,j) (i,j-1) (i-1,j)
 void iterate(){
+    int i, j;
     for(i = 0; i < l; i++)
-        for(j = 0; j < l; j++)
-            
+        for(j = 0; j < l; j++){
+            if(j+1 < l)
+                attemptFlip(i, j, i, j+1); //top
+            if(i+1 < l)
+                attemptFlip(i, j, i+1, j); //right
+            if(j-1 > 0)
+                attemptFlip(i, j, i, j-1); //bottom
+            if(i-1 > 0)
+                attemptFlip(i, j, i-1, j); //left
+        }
 }
 
-void calculateEnergy(int x, int y){
-    
+//in = neighbour's i, jn = neighbour's j
+void attemptFlip(int i, int j, int in, int jn){
+    int deltaE = computePairEnergy(i, j, in, jn);
+    if(deltaE < 0 || exp(-beta*deltaE) > randomDoubleInclusive(0.,1.)){
+        int temp = lattice[i][j];
+        lattice[i][j] = lattice[in][jn];
+        lattice[in][jn] = temp;
+    }
+}
+
+int computePairEnergy(int i, int j, int in, int jn){
+    return 2*J*(lattice[i][j]*computeEnergy(i, j, in, jn) + lattice[in][jn]*computeEnergy(in, jn, i, j));
+}
+
+//in = neighbour's i, jn = neighbour's j
+int computeEnergy(int i, int j, int in, int jn){
+    int energy = 0;
+    if(!(in == i && jn == j+1)) //top
+        energy += j+1 < l ? lattice[i][j+1] : -1; //keeps interface from wandering off
+    if(!(in == i+1 && jn == j)) //right
+        energy += i+1 < l ? lattice[i+1][j] : lattice[0][j]; //horizontal periodic boundary condition
+    if(!(in == i && jn == j-1)) //bottom
+        energy +=  j-1 > 0 ? lattice[i][j-1] : 1; //keeps interface from wandering off
+    if(!(in == i-1 && jn == j)) //left
+        energy += i-1 > 0 ? lattice[i-1][j] : lattice[l-1][j]; //horizontal periodic boundary condition
+    return energy;
 }
 
 void report(FILE *file){
@@ -76,6 +125,6 @@ void timeit(void (*fun)(void)){
     printf("%.6f\n", timeElapsed);
 }
 
-int randomIntegerInclusive(int lowerLimit, int upperLimit){
-    return lowerLimit + rand() % (upperLimit+1 - lowerLimit);
+double randomDoubleInclusive(double lowerLimit, double upperLimit){
+    return lowerLimit + (double)rand() / (double)RAND_MAX * (upperLimit - lowerLimit);
 }
