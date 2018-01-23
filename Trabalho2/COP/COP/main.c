32527a9281191a0f1012cfa3180044d60d0f7a64
diogofriggo <diogofriggobr@gmail.com>
2018-01-23 19:19:23 -0200

    Fixed a lot of issues but particles are drifting to the left, perhaps a random generator problem

--- Trabalho2/COP/COP/main.c
+++ Trabalho2/COP/COP/main.c
@@ -5,6 +5,9 @@
 #include <limits.h>
 #include <string.h>
 
+#define ATTEMPT_FLIP 0
+#define PRINT_LATTICE 0
+
 void run();
 void stats();
 void setup();
@@ -16,73 +19,60 @@ void report(FILE *file);
 void setup();
 void timeit(void (*fun)(void));
 double randomDoubleInclusive(double lowerLimit, double upperLimit);
-void printBoard();
+void printLattice();
 
 const double rho = 0.5;
 const int l = 50;
 const int l2 = l/2;
-const int iterations = 100;
+const int iterations = 40;
 const int J = 1;
 const int kB = 1;
 const int T = 1;
-//beta >= 187 forbids any transition
-//changing boundary conditions beta >= 94 forbids any transition
-int beta = 530000000;//1/(kB*T);
-
+double beta = 0.7 ;//1/(kB*T);
+const int offset = 12;
+double exps[25];
 int lattice[l][l];
-
+int getPBCIndex(int i);
+    
 int main() {
     srand((unsigned int)time(NULL));
-//    stats();
+    timeit(run);
     int i;
-    for(i = 1; i < 550000000; i += 1000000)
+    for(i = 1; i < 10; i++)
     {
-        beta = i;
+        beta = i/10.;
         run();
-        //timeit(run);
-        int beta_stable = 1;
-        int j;
-        for(j = 0; j < l; j++)
-            if(lattice[1][j] == -1){
-                beta_stable = 0;
-                break;
-            }
-        if(beta_stable)
-            printf("%.2f is stable\n", beta);
     }
-    
-//    int i;
-//    for(i = 0; i < 100; i++){
-//        printf("%.2f ", randomDoubleInclusive(0.,1.));
-//    }
-    return 0;
 }
 
 void run(){
     int i;
     setup();
-    const char path[] = "/Users/diogofriggo/Google Drive/UFRGS 8o Semestre/METODOS/metcompc/Trabalho2/COP/COP/cop.txt";
+    char path[300] = "/Users/diogofriggo/Google Drive/UFRGS 8o Semestre/METODOS/metcompc/Trabalho2/COP/COP/";
+    char name[50];
+    sprintf(name, "cop%.1f.txt", beta);
+    strcat(path, name);
     FILE *file = fopen(path, "w");
+    report(file);
     for(i = 0; i < iterations; i++){
         iterate();
         report(file);
-//        if(i%100==0)
-            //printBoard();
+        #if PRINT_LATTICE
+        printLattice();
+        #endif
     }
     fclose(file);
 }
 
-//void stats()
-//{
-//    double m = pow(1. - pow(1./sinh(2*beta*J), 2.), 1./8.);
-//    double pplus = (1.+m)/2.;
-//    double pminus = (1.-m)/2.;
-//    printf("m = %.2f, %.2f <= p <= %.2f\n", m, pminus, pplus);
-//}
-
 void setup()
 {
     int i, j;
+    //precompute possible exponentials to save CPU time
+    for(i = 0; i < 25; i++)
+        exps[i] = 0.;
+    int deltaEs[7] = {12, 8, 4, 0, -4, -8, -12};
+    for(i = 0; i < 7; i++)
+        exps[offset + deltaEs[i]] = exp(-beta*deltaEs[i]);
     
     for(i = 0; i < l; i++)
         for(j = 0; j < l; j++)
@@ -118,55 +108,69 @@ void iterate(){
         }
 }
 
+
+//TIP: find out the possible values of energy difference and estimate for what values of beta they would yield a non-zero, non-infinite exponential
+
 //in = neighbour's i, jn = neighbour's j
+//all indices are expected to be valid, that is, corrected for boundary conditions, before coming to this function
 void attemptFlip(int i, int j, int in, int jn){
+    if(lattice[i][j] == lattice[in][jn])
+        return;
     int deltaE = computePairEnergy(i, j, in, jn);
 //    printf("%d\n", deltaE);
-    if(deltaE < 0 || exp(-beta*deltaE) < randomDoubleInclusive(0.,1.)){
+//    printf("exps[12 + %d] = %.4f\n", deltaE, exps[offset + deltaE]);
+    double r = randomDoubleInclusive(0.,1.);
+    if(deltaE < 0 || exps[offset + deltaE] > r){
         int temp = lattice[i][j];
         lattice[i][j] = lattice[in][jn];
         lattice[in][jn] = temp;
+        #if ATTEMPT_FLIP
+        printf("success flip. ");
+        #endif
     }
+    #if ATTEMPT_FLIP
+    else
+        printf("failed flip. ");
+    printf("%d < 0 || %.4f >= %.4f) (%d,%d,%d) <-> (%d,%d,%d)\n", deltaE, exp(-beta*deltaE), r, i, j, lattice[i][j], in, jn, lattice[in][jn]);
+    if(deltaE > 1000 || deltaE < -1000)
+        printf("ERROR\nERROR\nERROR\nERROR\nERROR\nERROR\nERROR\nERROR\nERROR\nERROR\nERROR\nERROR\nERROR\nERROR\nERROR\n");
+    #endif
 }
 
 int computePairEnergy(int i, int j, int in, int jn){
     return 2*J*(lattice[i][j]*computeEnergy(i, j, in, jn) + lattice[in][jn]*computeEnergy(in, jn, i, j));
 }
 
+//shit is incomplete
 //in = neighbour's i, jn = neighbour's j
 int computeEnergy(int i, int j, int in, int jn){
     int energy = 0;
     //compute the energy of the neighbours of (i,j) excluding (in,jn)
-    //I think in != i && jn != (j+1) would also work
-    if(!(in == i && jn == (j+1))) //top
-    {
-        if((j+1) < (l-1)) //if not neighbour on the top layer
-            energy += lattice[i][j+1];
-        else
-            energy += -1; //boundary condition which keeps interface from wandering off
-    }
-    if(!(in == (i+1) && jn == j)) //right
+    if(in != i || jn != (j+1)) energy += lattice[i][j+1];
+    if(in != i || jn != (j-1)) energy += lattice[i][j-1];
+    if(in != getPBCIndex(i+1) || jn != j)
     {
         if((i+1) < l)
             energy += lattice[i+1][j];
         else
-            energy += lattice[0][j]; //horizontal periodic boundary condition
+            energy += lattice[0][j];
     }
-    if(!(in == i && jn == (j-1))) //bottom
+    if(in != getPBCIndex(i-1) || jn != j)
     {
-        if((j-1) > 0) //if not neighbour on the bottom layer
-            energy += lattice[i][j-1];
-        else
-            energy += 1; //boundary condition which keeps interface from wandering off
-    }
-    if(!(in == (i-1) && jn == j)) //left
-    {
-        if((i-1) > 0)
+        if((i-1) >= 0)
             energy += lattice[i-1][j];
         else
-            energy += lattice[l-1][j]; //horizontal periodic boundary condition
+            energy += lattice[l-1][j];
     }
     return energy;
+}
+
+int getPBCIndex(int i){
+    if(i < 0)
+        return 49;
+    if(i > (l-1))
+        return 0;
+    return i;
 }
 
 void report(FILE *file){
@@ -188,8 +192,8 @@ double randomDoubleInclusive(double lowerLimit, double upperLimit){
     return lowerLimit + (double)rand() / (double)RAND_MAX * (upperLimit - lowerLimit);
 }
 
-void printBoard(){
-    printf("Printing board...\n");
+void printLattice(){
+    printf("Printing lattice...\n");
     int i, j;
     for(j = l-1; j >= 0; j--){
         printf("%2d ", j);
@@ -197,7 +201,4 @@ void printBoard(){
             printf("%s", lattice[i][j] == 1 ? "u" : "-");
         printf("\n");
     }
-    printf("\n");
-    //I avoided this clever trick because it hinders readability:
-    //printf("%3d%s", board[i][j], j%ncols == (ncols-1) ? "\n" : "");
 }
